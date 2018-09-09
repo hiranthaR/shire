a1e49d72576ad4067920cb7f4a77d80872aed7ce
Hirantha <sahanhirantha@gmail.com>
2018-09-09 14:14:46 +0530

    menu tiles complete

--- lib/custom/widgets/home_screen/menu.dart
+++ lib/custom/widgets/home_screen/menu.dart
@@ -1,21 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:shire/utils/icons.dart';
 
 class HomeScreenMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 5.0),
       child: Wrap(
         children: <Widget>[
           GridView.count(
-            crossAxisCount: 2,
+            padding: EdgeInsets.only(left: 3.0, right: 3.0),
+            crossAxisCount:
+                MediaQuery.of(context).orientation == Orientation.portrait
+                    ? 2
+                    : 3,
             shrinkWrap: true,
             physics: NeverScrollableScrollPhysics(),
             children: <Widget>[
-
+              MenuTile(title: "Chatrooms", image: ShireIcons.CHAT_ICON),
+              MenuTile(title: "Inbox", image: ShireIcons.INBOX_ICON),
             ],
           )
         ],
+      ),
+    );
+  }
+}
+
+class MenuTile extends StatelessWidget {
+  final String title;
+  final String image;
+
+  MenuTile({@required this.title, @required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      margin: EdgeInsets.all(5.0),
+      padding: EdgeInsets.all(5.0),
+      child: SizedBox.expand(
+        child: RaisedButton(
+          child: Column(
+            children: <Widget>[
+              Container(height:130.0,padding: EdgeInsets.all(5.0),child: Image.asset(image)),
+              Text(title),
+            ],
+          ),
+          onPressed: () {
+            print("ddsf");
+          },
+        ),
       ),
     );
   }
--- lib/utils/icons.dart
+++ lib/utils/icons.dart
@@ -1,3 +1,5 @@
 class ShireIcons{
   static const GOOGLE_ICON = "assets/icons/google_icon.png";
+  static const CHAT_ICON = "assets/icons/chat.png";
+  static const INBOX_ICON = "assets/icons/inbox.png";
 }
